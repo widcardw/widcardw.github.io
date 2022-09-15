de6effd9522d56c6871a64b8a71ec280a10be436
widcardw <lui747353367@qq.com>
2022-09-15 23:27:08 +0800

    fix: use `vite-plugin-vue-markdown`

--- unocss.config.ts
+++ unocss.config.ts
@@ -3,7 +3,6 @@ import {
   presetAttributify,
   presetIcons,
   presetUno,
-  presetWebFonts,
   // transformerDirectives,
   // transformerVariantGroup,
 } from 'unocss'
@@ -20,13 +19,6 @@ export default defineConfig({
     presetIcons({
       scale: 1.2,
       warn: true,
-    }),
-    presetWebFonts({
-      fonts: {
-        sans: 'DM Sans',
-        serif: 'DM Serif Display',
-        mono: 'Jetbrains Mono',
-      },
     }),
   ],
   // transformers: [
