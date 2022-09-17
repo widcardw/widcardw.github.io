df9d05e5f445488bb66a330ce15d23eeb24ba022
widcardw <lui747353367@qq.com>
2022-09-17 22:38:15 +0800

    feat: add nprogress

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,7 @@ import './styles/callouts.css'
 import './styles/heti-ext.css'
 import 'uno.css'
 import './styles/heti.css'
+import type { UserModule } from './types'
 
 const routes = setupLayouts(generatedRoutes)
 
@@ -27,4 +28,9 @@ const routes = setupLayouts(generatedRoutes)
 export const createApp = ViteSSG(
   App,
   { routes, base: import.meta.env.BASE_URL },
+  (ctx) => {
+    // install all modules under `modules/`
+    Object.values(import.meta.glob<{ install: UserModule }>('./modules/*.ts', { eager: true }))
+      .forEach(i => i.install?.(ctx))
+  },
 )
