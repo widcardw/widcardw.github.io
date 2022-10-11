46b9af874b8d039e271c0063fdf79ff98799970c
widcardw <55699713+widcardw@users.noreply.github.com>
2022-10-11 04:15:03 +0000

    Deploying to gh-pages from @ widcardw/widcardw.github.io@be59f3df233177b9165aaaf419d7fb7f8f8efc83 🚀

---
+++ assets/home.fabf2fa7.js
@@ -0,0 +1,1 @@
+import{H as s,o as c,h as a,d as e,a as n,I as r,s as _}from"./app.5eb1a95f.js";const l={},m={class:"text-center text-gray-700 dark:text-gray-200 flex flex-col h-full"},d={"flex-1":"","mt-3rem":""};function f(i,x){const t=r,o=_("RouterView");return c(),a("main",m,[e(t),n("div",d,[e(o)])])}const h=s(l,[["render",f]]);export{h as default};
