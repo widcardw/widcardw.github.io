a2a756368a6136bdd8315b658c38bba69fb80534
widcardw <lui747353367@qq.com>
2022-10-12 00:10:01 +0800

    chore: add background random seeds

--- src/components/client/Maze.vue
+++ src/components/client/Maze.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
+import { storeToRefs } from 'pinia'
+import { useGlStore } from '~/stores/gl'
+
+const { seeds } = storeToRefs(useGlStore())
 // code from https://www.bilibili.com/video/BV1ce411g7B2
 const shaderCode = computed(() => `
   const float C = 0.707107;
 
   float noise(vec2 x) {
-      return abs(fract(114514.114514 * sin(dot(x, vec2(114., 514.)))));
+      return abs(fract(${seeds.value[0]} * sin(dot(x, vec2(${seeds.value[1]}, ${seeds.value[2]})))));
   }
 
   float line(vec2 p, float dir) {
@@ -14,7 +18,7 @@ const shaderCode = computed(() => `
 
   void main() {
       vec2 uv = 20. * (gl_FragCoord.xy - .5 * iResolution.xy) / min(iResolution.x, iResolution.y);
-      uv += iTime / 5.;
+      uv += iTime / 5. * vec2(${parseFloat(seeds.value[0]) > 500 ? '1.' : '-1.'}, ${parseFloat(seeds.value[1]) > 497 ? '-1.' : '1.'});
       vec3 color = vec3(line(fract(uv) - .5, noise(floor(uv))));
       ${isDark.value ? 'vec3 c = mix(vec3(0.1), vec3(0.125), color)' : 'vec3 c = mix(vec3(1.0), vec3(0.94), color)'};
       gl_FragColor = vec4(c, 1.);
--- src/stores/gl.ts
+++ src/stores/gl.ts
@@ -3,8 +3,15 @@ import { acceptHMRUpdate, defineStore } from 'pinia'
 export const useGlStore = defineStore('gl', () => {
   const isLoaded = ref(false)
 
+  const seeds = ref([
+    Math.random() * 1000,
+    Math.random() * 987,
+    Math.random() * 654,
+  ].map(i => i.toFixed(2)))
+
   return {
     isLoaded,
+    seeds,
   }
 })
 
