91f0ae426465cd2d5b7b5eb1d9cb3d68fb0faa0d
widcardw <lui747353367@qq.com>
2023-01-09 14:48:12 +0800

    chore: add link

--- src/components/client/FriendLinks.vue
+++ src/components/client/FriendLinks.vue
@@ -1,35 +1,5 @@
 <script setup lang="ts">
-const friends = [{
-  name: '鹤翔万里',
-  avatar: '/avatar/tonycrane.png',
-  desc: '翔哥永不咕！',
-  link: 'http://blog.tonycrane.cc/',
-}, {
-  name: 'GZTime',
-  avatar: '/avatar/GZTime_2021.png',
-  desc: 'Walking on the Time Axis.',
-  link: 'https://blog.gztime.cc',
-}, {
-  name: 'Matheart',
-  avatar: '/avatar/matheart.webp',
-  desc: 'Math Heart Ever.',
-  link: 'https://matheart.github.io/',
-}, {
-  name: '范滇东',
-  avatar: '/avatar/flwfdd.jpeg',
-  desc: '三体 × 摄影 × 造物',
-  link: 'https://flwfdd.xyz/',
-}, {
-  name: 'HK-SHAO',
-  avatar: '/avatar/hk-shao.png',
-  desc: 'A human.',
-  link: 'https://shao.fun/',
-}, {
-  name: '你们的小f',
-  avatar: '/avatar/fzf404.jpeg',
-  desc: '修炼中',
-  link: 'https://www.fzf404.art/',
-}]
+import { friends } from '~/data/links.json'
 </script>
 
 <template>
